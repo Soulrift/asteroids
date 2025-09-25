5defeda8ff7241b881dd50df3e78ea448d24ed90
Soulrift <soulriftx@gmail.com>
2025-09-25 17:50:47 -0500

    player init

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from constants import *
+
+from constants import *  # noqa: F403
+from player import *
 
 
 def main():
@@ -7,9 +9,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
