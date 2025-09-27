1cc816510cb5049660d15eb2b50ea2dacbd742d5
Soulrift <soulriftx@gmail.com>
2025-09-27 18:03:59 -0500

    asteroid classes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *  # noqa: F403
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -11,12 +13,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
